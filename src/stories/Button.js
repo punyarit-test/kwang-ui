3374ad2eb9f0b6cc52d1266bf8aae2e00a5f42a5
Kwang <punyarit.thi@hlabconsulting.com>
2022-12-15 21:12:55 +0700

    test button

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -27,7 +27,7 @@ export const Button = ({
       style=${styleMap({backgroundColor})}
       @click=${onClick}
     >
-      ${label} 55 666666666
+      ${label} 55 666666666 test
     </button>
   `;
 };
